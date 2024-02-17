fb3422cf9fc01a49e574b8c3297651145ba692e3
rahul6967 <rahudahiya6967@gmail.com>
2024-02-17 14:15:26 +0530

    first commit

---
+++ Employee_Manager/urls.py
@@ -0,0 +1,44 @@
+"""Employee_Manager URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from employee import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('',views.index,name='index'),
+    path('registration/',views.registration,name='registration'),
+    path('emp_login/',views.emp_login,name='emp_login'),
+    path('emp_home/',views.emp_home,name='emp_home'),
+    path('profile/',views.profile,name='profile'),
+    path('logout/',views.Logout,name='logout'),
+    path('admin_login/',views.admin_login,name='admin_login'),
+    path('my_experience/',views.my_experience,name='my_experience'),
+    path('edit_myexperience/',views.edit_myexperience,name='edit_myexperience'),
+    path('my_education/',views.my_education,name='my_education'),
+    path('edit_myeducation/',views.edit_myeducation,name='edit_myeducation'),
+    path('change_password/',views.change_password,name='change_password'),
+    path('admin_home/',views.admin_home,name='admin_home'),
+    path('change_passwordadmin/',views.change_passwordadmin,name='change_passwordadmin'),
+    path('all_employee/',views.all_employee,name='all_employee'),
+    path('edit_profile/<int:pid>/', views.edit_profile, name='edit_profile'),
+    path('edit_education/<int:pid>/', views.edit_education, name='edit_education'),
+    path('edit_experience/<int:pid>/', views.edit_experience, name='edit_experience'),
+    path('delete_employee/<int:pid>/', views.delete_employee, name='delete_employee'),
+
+
+    
+]
